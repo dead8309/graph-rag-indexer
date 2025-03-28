6c7021d876d01bd4243ce83d4269e53210b4e6ca
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2025-03-28 16:32:45 +0530

    fix: file paths

--- src/config.py
+++ src/config.py
@@ -13,8 +13,9 @@ NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 
+# PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # codebase for indexing
-CODEBASE_DIR = os.path.join("samples_typescript", "express_mongodb")
+CODEBASE_DIR = os.path.join("samples-typescript", "express-mongodb")
 
 if not NEO4J_PASSWORD:
     raise ValueError("NEO4J_PASSWORD is required")
