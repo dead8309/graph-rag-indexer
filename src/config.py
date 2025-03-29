bfb0ac1cb7863a2419cc308602bb33508ceeae93
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2025-03-29 10:24:31 +0530

    update config

--- src/config.py
+++ src/config.py
@@ -7,6 +7,18 @@ load_dotenv()
 MILVUS_HOST = os.getenv("MILVUS_HOST", "localhost")
 MILVUS_PORT = os.getenv("MILVUS_PORT", 19530)
 MILVUS_ALIAS = os.getenv("MILVUS_ALIAS", "default")
+MILVUS_COLLECTION_NAME = os.getenv("MILVUS_COLLECTION_NAME", "indexer")
+MILVUS_TEXT_FIELD = os.getenv("MILVUS_TEXT_FIELD", "text")
+MILVUS_VECTOR_FIELD = os.getenv("MILVUS_VECTOR_FIELD", "vector")
+MILVUS_ID_FIELD = os.getenv("MILVUS_ID_FIELD", "id")
+
+MILVUS_METRIC_TYPE = "COSINE"
+MILVUS_INDEX_TYPE = "IVF_FLAT"
+MILVUS_NLIST = 16384
+MILBUS_NPROBE = 16
+
+
+VECTOR_SEARCH_TOP_K = 3
 
 # Neo4j Config
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
@@ -25,5 +37,8 @@ if not NEO4J_PASSWORD:
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_EMBEDDING_MODEL = os.getenv("OPENAI_EMBEDDING_MODEL", "text-embedding-3-small")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+EMBEDDING_DIM = os.getenv("EMBEDDING_DIM", 1536)
+
 
 print("Config loaded")
